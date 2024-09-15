328b39c7067b92f47bd5fd12a823da3023a1897f
Eric <ericmichaels@gmail.com>
2024-09-15 00:27:42 -0700

    added text_node_to_html_node and unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,6 @@
 import unittest
-
-from textnode import TextNode
-
+from textnode import TextNode, text_node_to_html_node, TextType
+from htmlnode import LeafNode, ParentNode
 
 class TestTextNode(unittest.TestCase):
 
@@ -28,10 +27,43 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         self.assertEqual(repr(node), "TextNode(This is a text node, bold, None)")
 
-    
 
-    
+class TestTextToHTML(unittest.TestCase):
+    def test_text_node_to_html_node_text(self):
+        node = TextNode("Hello", TextType.text)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), "Hello")
 
+    def test_text_node_to_html_node_bold(self):
+        node = TextNode("Hello", TextType.bold)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, ParentNode)
+        self.assertEqual(html_node.to_html(), "<b>Hello</b>")
+
+    def test_text_node_to_html_node_italic(self):
+        node = TextNode("Hello", TextType.italic)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, ParentNode)
+        self.assertEqual(html_node.to_html(), "<i>Hello</i>")
+
+    def test_text_node_to_html_node_code(self):
+        node = TextNode("Hello", TextType.code)
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, ParentNode)
+        self.assertEqual(html_node.to_html(), "<code>Hello</code>")
+
+    def test_text_node_to_html_node_link(self):
+        node = TextNode("Hello", TextType.link, url="http://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, ParentNode)
+        self.assertEqual(html_node.to_html(), '<a href="http://example.com">Hello</a>')
+
+    def test_text_node_to_html_node_image(self):
+        node = TextNode("Image", TextType.image, url="http://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertIsInstance(html_node, LeafNode)
+        self.assertEqual(html_node.to_html(), '<img src="http://example.com/image.png" alt="Image"></img>')
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,7 @@
+from enum import Enum
+from htmlnode import LeafNode, ParentNode
+
+TextType = Enum("TextType", ["text", "bold", "italic", "code", "link", "image"])
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -12,3 +16,20 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.text:
+            return LeafNode(text_node.text)
+        case TextType.bold:
+            return ParentNode("b", [LeafNode(text_node.text)])
+        case TextType.italic:
+            return ParentNode("i", [LeafNode(text_node.text)])
+        case TextType.code:
+            return ParentNode("code", [LeafNode(text_node.text)])
+        case TextType.link:
+            return ParentNode("a", [LeafNode(text_node.text)], {"href": text_node.url})
+        case TextType.image:
+            return LeafNode("", "img", {"src": text_node.url, "alt": text_node.text})
+
