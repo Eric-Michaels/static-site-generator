98166cd04d635f77edf7df2187809dc398e8147b
Eric <ericmichaels@gmail.com>
2024-09-16 13:03:20 -0700

    added extract_markdown_images and _links functions to splitnodes, wrote unit tests for both functions.

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -29,3 +30,21 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
     
     return new_nodes
+
+
+def extract_markdown_images(text):
+    image_tuples = re.findall(r'!\[(.*?)\]\((.*?)\)', text)
+    if len(image_tuples) == 0:
+        raise ValueError("No image found")
+    else:
+        return image_tuples
+
+def extract_markdown_links(text):
+    link_tuples = re.findall(r'(?<!\!)\[(.*?)\]\((.*?)\)', text)
+    if len(link_tuples) == 0:
+        raise ValueError("No link found")
+    else:
+        return link_tuples
+
+    
+    
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from splitnodes import split_nodes_delimiter
+from splitnodes import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestSplitNodesDelimiter(unittest.TestCase):
 
@@ -85,6 +85,58 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             TextNode(" text", TextType.text),
         ]
         self.assertEqual(new_nodes, expected_nodes)
+
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images_single(self):
+        text = "![alt text](image.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt text", "image.jpg")])
+
+    def test_extract_markdown_images_multiple(self):
+        text = "![alt text](image1.jpg) ![another alt](image2.png)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt text", "image1.jpg"), ("another alt", "image2.png")])
+
+    def test_extract_markdown_images_none(self):
+        text = "No images here!"
+        with self.assertRaises(ValueError):
+            extract_markdown_images(text)
+
+    def test_extract_markdown_images_mixed(self):
+        text = "![alt text](image.jpg) [link](url)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt text", "image.jpg")])
+
+    def test_extract_markdown_images_malformed(self):
+        text = "![alt text(image.jpg)"
+        with self.assertRaises(ValueError):
+            extract_markdown_images(text)
+
+    def test_extract_markdown_links_single(self):
+        text = "[link text](url)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link text", "url")])
+
+    def test_extract_markdown_links_multiple(self):
+        text = "[link1](url1) [link2](url2)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link1", "url1"), ("link2", "url2")])
+
+    def test_extract_markdown_links_none(self):
+        text = "No links here!"
+        with self.assertRaises(ValueError):
+            extract_markdown_links(text)
+
+    def test_extract_markdown_links_mixed(self):
+        text = "[link text](url) ![alt text](image.jpg)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link text", "url")])
+
+    def test_extract_markdown_links_malformed(self):
+        text = "[link text(url)"
+        with self.assertRaises(ValueError):
+            extract_markdown_links(text)
 
 if __name__ == '__main__':
     unittest.main()
