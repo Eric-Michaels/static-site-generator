7e857542985097cccf728b34ad5cbf106b1bb523
Eric <ericmichaels@gmail.com>
2024-09-16 23:00:49 -0700

    added text_to_textnodes(text) function and wrote unit tests for it.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from textnode import TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from splitnodes import split_nodes_delimiter
+from splitnodes import split_nodes_delimiter, split_nodes_image, split_nodes_link, extract_markdown_links, extract_markdown_images, text_to_textnodes
 
 def main():
     
@@ -18,6 +18,10 @@ def main():
 
     split_nodes_delimiter([TextNode("Hello", "text"), TextNode("world", "bold")], "*", "italic")
     print(split_nodes_delimiter([TextNode("Hello", "text"), TextNode("world", "bold")], "*", "italic"))
+
+    text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    print(":::::::::::::::::::::::::::::::::::")
+    print(text_to_textnodes(text))
 
 # Using the special variable 
 # __name__
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -5,6 +5,7 @@ from splitnodes import (
     split_nodes_link,
     extract_markdown_links,
     extract_markdown_images,
+    text_to_textnodes
 )
 
 from textnode import (
@@ -18,10 +19,11 @@ from textnode import (
 )
 
 
-class TestInlineMarkdown(unittest.TestCase):
+class TestSplitNodes(unittest.TestCase):
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", text_type_text)
         new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        print(f"new_nodes: {new_nodes}")
         self.assertListEqual(
             [
                 TextNode("This is text with a ", text_type_text),
@@ -165,6 +167,64 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is text with a **bolded** word and *italic* word and `code block` word and ![image](https://i.imgur.com/zjjcJKZ.png) and [link](https://boot.dev)"
+        )
+        print(f"nodes: {nodes}")
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word and ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word and ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
+            ],
+            nodes,
+        )
+        
+    def test_text_to_textnodes_combined(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is ", text_type_text),
+            TextNode("text", text_type_bold),
+            TextNode(" with an ", text_type_text),
+            TextNode("italic", text_type_italic),
+            TextNode(" word and a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" and an ", text_type_text),
+            TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", text_type_text),
+            TextNode("link", text_type_link, "https://boot.dev"),
+        ]
+        self.assertListEqual(expected_nodes, nodes)
+
+    def test_text_to_textnodes_multiple_links(self):
+        text = "This is text with an italic word and a code block and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and [link1](https://boot.dev) and [link2](https://test2.com)"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is text with an italic word and a code block and an ", text_type_text),
+            TextNode("obi wan image", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and ", text_type_text),
+            TextNode("link1", text_type_link, "https://boot.dev"),
+            TextNode(" and ", text_type_text),
+            TextNode("link2", text_type_link, "https://test2.com"),
+        ]
+        self.assertListEqual(expected_nodes, nodes)
+
+    def test_text_to_textnodes_no_links(self):
+        text = "This is text with an italic word and a code block"
+        nodes = text_to_textnodes(text)
+        expected_nodes = [
+            TextNode("This is text with an italic word and a code block", text_type_text)
+        ]
+        self.assertListEqual(expected_nodes, nodes)
 
 if __name__ == "__main__":
     unittest.main()
