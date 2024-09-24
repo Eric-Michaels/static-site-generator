ad8835912207349a763b65d520d9d7a67ef9bc8c
Eric <ericmichaels@gmail.com>
2024-09-24 11:18:40 -0700

    added generate_pages_recursive to generate page for each .md file nested in source.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     src = "./static"
 
     copy_files(src, dst)
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content/", "./template.html", "./public/")
 
 
 def extract_title(markdown):
@@ -25,8 +25,19 @@ def generate_page(from_path, template_path, dest_path):
         template_content = file.read()
     html_content = markdown_to_html_node(src_content)
     title = extract_title(src_content)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as file:
         file.write(template_content.replace("{{ Content }}", html_content).replace("{{ Title }}", title))
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for root, dirs, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                rel_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dest_dir_path, os.path.splitext(rel_path)[0] + ".html")
+                generate_page(from_path, template_path, dest_path)
     
 
 
