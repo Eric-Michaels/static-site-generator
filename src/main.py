fe89b3055183d907b6df1a2d051c1e84d0a76372
Eric <ericmichaels@gmail.com>
2024-09-24 00:24:01 -0700

    added function to remove public dir then copy static to clean public dir

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
-from textnode import TextNode
-from htmlnode import HTMLNode, LeafNode, ParentNode
-from splitnodes import split_nodes_delimiter
-from splitblocks import markdown_to_blocks
-from markdowntohtml import markdown_to_html_node
+import os
+import shutil
 
 def main():
+    dst = "./public"
+    src = "./static"
+
+    copy_files(src, dst)
+
+def copy_files(src, dst):
+    clean_destination(dst)
+    shutil.copytree(src, dst, copy_function=copy_and_print)
+
+def clean_destination(dst):
+    # Remove the directory if it exists
+    if os.path.exists(dst):
+        shutil.rmtree(dst, ignore_errors=True)
+
+def copy_and_print(src, dst):
+    print(f'Copying {src} to {dst}')
+    return shutil.copy2(src, dst)
     
 
 # Using the special variable 
